61124af7f4e9dff9b5f73bac6f91ae5b07fc5f88
najs <na.jss@daum.net>
2016-12-27 17:07:19 +0900

    modify

--- slide/js/jsSlide5.js
+++ slide/js/jsSlide5.js
@@ -20,17 +20,15 @@ var jsSlide = (function(){
 			btnStopClass : options.btnStop || '.jsSlide-btn-stop',
 
 			slideListClass : options.slideListClass ||'.jsSlide-list',
-
 			autoDuration : options.autoDuration || 1000,
 			effectMode : options.effectMode || 'normal', //'sliding','fade'
 			effectSpeed : options.effectSpeed || 0,
-
 			pastIdx : 0,
 			presentIdx : options.firstIdx || 0,
 			loop : options.loooop || false,
-
 			timer : {},
-			timerSpeed : options.timerSpeed || 3000
+			timerSpeed : options.timerSpeed || 3000,
+			isPlay : true
 		};
 
 		this.set = {
@@ -53,28 +51,28 @@ var jsSlide = (function(){
 
 
 		this.set.btnNextEle.on('click',function(){
+			if(!$this.defaults.isPlay) return;
 			$this.next();
-			$this.autoStop();
 			$this.autoPlay();
 		});
 
 		this.set.btnPrevEle.on('click',function(){
+			if(!$this.defaults.isPlay) return;
 			$this.previous();
-			$this.autoStop();
 			$this.autoPlay();
 		});
 
 		this.set.pageEle.on('click',function(e){
 			e.preventDefault();
+			if(!$this.defaults.isPlay) return;
 			var selfIdx = $(this).index();
 			$this.reset(selfIdx);
-			$this.autoStop();
 			$this.autoPlay();
 		});
 
 		this.set.btnStopEle.on('click',function(){
-			$this.autoStop();
 
+			$this.autoControl();
 		});
 	};
 
@@ -94,10 +92,12 @@ var jsSlide = (function(){
 		}else{
 			this.defaults.presentIdx = this.defaults.presentIdx <= 0 ? this.set.slideEleLength-1 : this.defaults.presentIdx-1;
 		}
+
+
 		this.slideChange('prev',this.defaults.effectMode);
 		this.pagingChange();
 
-		/*console.log('presentIdx :'+this.defaults.presentIdx ,'pastIdx :'+this.defaults.pastIdx);*/
+		console.log('presentIdx :'+this.defaults.presentIdx ,'pastIdx :'+this.defaults.pastIdx);
 	};
 
 	jsSlide.prototype.next = function(){
@@ -111,7 +111,7 @@ var jsSlide = (function(){
 		this.slideChange('next',this.defaults.effectMode);
 		this.pagingChange();
 
-/*		console.log('presentIdx :'+this.defaults.presentIdx ,'pastIdx :'+this.defaults.pastIdx);*/
+		console.log('presentIdx :'+this.defaults.presentIdx ,'pastIdx :'+this.defaults.pastIdx);
 	};
 
 	jsSlide.prototype.reset = function(k){
@@ -121,7 +121,7 @@ var jsSlide = (function(){
 		this.slideChange('combination',this.defaults.effectMode);
 		this.pagingChange();
 
-		/*console.log('presentIdx :'+this.defaults.presentIdx, 'pastIdx :'+this.defaults.pastIdx);*/
+		console.log('presentIdx :'+this.defaults.presentIdx, 'pastIdx :'+this.defaults.pastIdx);
 	};
 
 	jsSlide.prototype.slideChange = function(dir,mode){
@@ -133,29 +133,45 @@ var jsSlide = (function(){
 				$this.set.slideEle.eq($this.defaults.presentIdx).css({'display':'block'});
 			},
 			fade:function(){
-				$this.set.slideEle.eq($this.defaults.pastIdx).stop(true, true).fadeOut($this.defaults.effectSpeed);
-				$this.set.slideEle.eq($this.defaults.presentIdx).stop(true, true).fadeIn($this.defaults.effectSpeed);
+				if(!$this.defaults.isPlay) return;
+				$this.defaults.isPlay = false;
+				$this.set.slideEle.eq($this.defaults.pastIdx).stop(true, true).fadeOut($this.defaults.effectSpeed,function(){
+					$this.defaults.isPlay = true;
+				});
+				$this.set.slideEle.eq($this.defaults.presentIdx).stop(true, true).fadeIn($this.defaults.effectSpeed,function(){
+					$this.defaults.isPlay = true;
+				});
 			},
 			sliding:{
 				next : function(){
+					if(!$this.defaults.isPlay) return;
+					$this.defaults.isPlay = false;
 					$this.set.slideEle.eq($this.defaults.pastIdx)
 						.css({'display':'block', 'left':'0'})
 						.stop(true, true).animate({'left':'-100%'}, $this.defaults.effectSpeed, function(){
 						$(this).css({'display':'none'});
+						$this.defaults.isPlay = true;
 					});
 					$this.set.slideEle.eq($this.defaults.presentIdx)
 						.css({'display':'block', 'left':'100%'})
-						.stop(true, true).animate({'left':0}, $this.defaults.effectSpeed);
+						.stop(true, true).animate({'left':0}, $this.defaults.effectSpeed,function(){
+							$this.defaults.isPlay = true;
+					});
 				},
 				prev : function(){
+					if(!$this.defaults.isPlay) return;
+					$this.defaults.isPlay = false;
 					$this.set.slideEle.eq($this.defaults.pastIdx)
 						.css({'display':'block', 'left':'0'})
 						.stop(true, true).animate({'left':'100%'}, $this.defaults.effectSpeed, function(){
 						$(this).css({'display':'none'});
+						$this.defaults.isPlay = true;
 					});
 					$this.set.slideEle.eq($this.defaults.presentIdx)
 						.css({'display':'block', 'left':'-100%'})
-						.stop(true, true).animate({'left':0}, $this.defaults.effectSpeed);
+						.stop(true, true).animate({'left':0}, $this.defaults.effectSpeed,function(){
+							$this.defaults.isPlay = true;
+					});
 				}
 			}
 		};
@@ -209,23 +225,45 @@ var jsSlide = (function(){
 	};
 
 	jsSlide.prototype.pagingChange = function(){
+
 		this.set.pageEle.eq(this.defaults.pastIdx).removeClass('on');
 		this.set.pageEle.eq(this.defaults.presentIdx).addClass('on');
 	};
 
 	jsSlide.prototype.autoPlay = function(){
-		if(this.defaults.auto && this.defaults.loop){
+		if(!this.defaults.loop){
+			this.set.btnStopEle.remove();
+			return;
+		}
+
+		if(this.defaults.auto){
+			this.set.btnStopEle.html('재생중!');
 			var $this = this;
 			clearInterval(this.timer);
 			this.timer = setInterval(function(){
 				$this.next();
 			},this.defaults.timerSpeed)
+
 		}
 	};
 
 	jsSlide.prototype.autoStop = function(){
+		this.set.btnStopEle.html('멈춤!');
 		clearInterval(this.timer);
-	}
+
+	};
+
+	jsSlide.prototype.autoControl = function(){
+		if(this.defaults.auto){
+			this.defaults.auto = false;
+			this.autoStop();
+		}else{
+			this.defaults.auto = true;
+			this.autoPlay();
+
+		}
+		console.log(this.defaults.auto)
+	};
 
 	return jsSlide;
 }());
